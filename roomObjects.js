ec5e795bfd3c594cb614380306fc6dceee59fb05
robmitch802 <60521912+robmitch802@users.noreply.github.com>
2020-02-13 13:34:26 -0500

    adding the room object test file

---
+++ roomObjects.js
@@ -0,0 +1,12 @@
+//2.12.20 Project Zorkington
+// rob building room object class(es)
+
+class Room {
+    constructor(title, descript) {
+        this.title = title
+        this.descript = descript
+        
+    }
+
+}
+
